d2c3ca70d003e139c8b55516dce8283742d81cfe
novenary <novenary@kwak.zip>
2024-11-02 01:35:59 +0200

    Set default platform to 1.20.1-forge

--- src/main/java/dev/isxander/controlify/DefaultMixinPlugin.java
+++ src/main/java/dev/isxander/controlify/DefaultMixinPlugin.java
@@ -13,18 +13,18 @@ public class DefaultMixinPlugin implements IMixinConfigPlugin {
     public DefaultMixinPlugin() {
         boolean areModsLoading = true;
         //? if fabric {
-        net.fabricmc.loader.api.FabricLoader fabricLoader = net.fabricmc.loader.api.FabricLoader.getInstance();
-        //?} elif neoforge {
+        /*net.fabricmc.loader.api.FabricLoader fabricLoader = net.fabricmc.loader.api.FabricLoader.getInstance();
+        *///?} elif neoforge {
         /*net.neoforged.fml.loading.LoadingModList loadingModList = net.neoforged.fml.loading.LoadingModList.get();
         *///?} elif forge {
-        /*net.minecraftforge.fml.loading.LoadingModList loadingModList = net.minecraftforge.fml.loading.LoadingModList.get();
-        *///?}
+        net.minecraftforge.fml.loading.LoadingModList loadingModList = net.minecraftforge.fml.loading.LoadingModList.get();
+        //?}
         for (final String modId : this.getModIds()) {
             //? if fabric {
-            areModsLoading = areModsLoading && fabricLoader.isModLoaded(modId);
-            //?} elif forgelike {
-            /*areModsLoading = areModsLoading && loadingModList.getModFileById(modId) != null;
-            *///?}
+            /*areModsLoading = areModsLoading && fabricLoader.isModLoaded(modId);
+            *///?} elif forgelike {
+            areModsLoading = areModsLoading && loadingModList.getModFileById(modId) != null;
+            //?}
         }
         this.areModsLoading = areModsLoading;
     }
